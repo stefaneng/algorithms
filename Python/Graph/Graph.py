a9848afdb6dd604b1c45f59bb334a9a79a55bfbc
Stefan Eng <stefaneng13@gmail.com>
2014-11-30 13:48:15 -0800

    Simple (undirected) graph data structure

--- Python/Graph/Graph.py
+++ Python/Graph/Graph.py
@@ -1,4 +1,16 @@
 class Graph(object):
-    "Graph data structure"
+    "(Undirected) Graph data structure (using Adjacency Lists implemented with a dict)"
     def __init__(self):
-        pass
+        self.adj = {}
+
+    def insert_vertex(self, v):
+        "Inserts vertex `x` into the graph"
+        if v not in self.adj:
+            self.adj[v] = []
+        return self.adj[v]
+
+    def insert_edge(self, u, v):
+        self.insert_vertex(u)
+        self.insert_vertex(v)
+        self.adj[u].append(v)
+        self.adj[v].append(u)
