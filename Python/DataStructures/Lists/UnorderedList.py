3fa4be49f04b51626facc51dddc756bcced6af74
Stefan Eng <stefaneng13@gmail.com>
2014-12-01 10:51:21 -0800

    Unordered list template of api

---
+++ Python/DataStructures/Lists/UnorderedList.py
@@ -0,0 +1,35 @@
+class List(object):
+    "Unordered list data structure"
+
+    def __init__(self):
+        self.head = None
+
+    def add(item):
+        pass
+
+    def remove(item):
+        pass
+
+    def search(item):
+        pass
+
+    def is_empty():
+        pass
+
+    def size():
+        pass
+
+    def append(item):
+        pass
+
+    def index(item):
+        pass
+
+    def insert(pos,item):
+        pass
+
+    def pop():
+        pass
+
+    def pop(pos):
+        pass
---
+++ Python/DataStructures/Lists/__init__.py
@@ -0,0 +1,1 @@
+from UnorderedList import List
